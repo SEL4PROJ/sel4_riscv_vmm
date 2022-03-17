5ca2c2707653015bb40c7f3aaf2e79b529a43e37
Ivan Velickovic <i.velickovic@unsw.edu.au>
2022-03-17 13:53:37 +1100

    Fix run scripts for newer QEMU versions (>= v5.1).
    QEMU 5.1 changed the default behavior from `-bios none` to
    `-bios default` which means these commands no longer work on current
    versions of QEMU, the default behavior will load QEMU's default OpenSBI
    firmware, instead of the firmware produced by the project's build system.
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-qemu-system-riscv64 -machine virt -cpu rv64,x-h=true -nographic -serial mon:stdio -m size=4095M -kernel images/sel4-riscv-vmm-image-riscv-spike
+qemu-system-riscv64 -bios none -machine virt -cpu rv64,x-h=true -nographic -serial mon:stdio -m size=4095M -kernel images/sel4-riscv-vmm-image-riscv-spike
 
